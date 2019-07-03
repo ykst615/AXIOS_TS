c02728103fcf5108af97be688f09d1944551147e
ykst <ykst615@qq.com>
2019-07-03 09:15:02 +0800

    feat(src/helpers/headers.js src/types/index.ts src/index.ts src/xhr.ts): Support for modifying reque

--- examples/base/app.ts
+++ examples/base/app.ts
@@ -70,17 +70,17 @@ axios({
   }
 })
 
-// axios({
-//   method: 'post',
-//   url: '/base/post',
-//   headers: {
-//     'content-type': 'application/json;charset=utf-8'
-//   },
-//   data: {
-//     a: 1,
-//     b: 2
-//   }
-// })
+axios({
+  method: 'post',
+  url: '/base/post',
+  headers: {
+    'content-type': 'application/json;charset=utf-8'
+  },
+  data: {
+    a: 1,
+    b: 2
+  }
+})
 
 const arr = new Int32Array([21, 31])
 
@@ -91,14 +91,14 @@ axios({
 })
 
 
-// const paramsString = 'q=URLUtils.searchParams&topic=api'
-// const searchParams = new URLSearchParams(paramsString)
+const paramsString = 'q=URLUtils.searchParams&topic=api'
+const searchParams = new URLSearchParams(paramsString)
 
-// axios({
-//   method: 'post',
-//   url: '/base/post',
-//   data: searchParams
-// })
+axios({
+  method: 'post',
+  url: '/base/post',
+  data: searchParams
+})
 
 // axios({
 //   method: 'post',
--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import { AxiosRequestConfig } from './types'
 import xhr from './xhr'
 import { buildURL } from './helpers/url'
 import { transformRequest } from './helpers/data'
+import { processHeaders } from './helpers/headers'
 
 function axios(config: AxiosRequestConfig): void {
   processConfig(config)
@@ -11,6 +12,7 @@ function axios(config: AxiosRequestConfig): void {
 function processConfig(config: AxiosRequestConfig): void {
   // url 转换
   config.url = transformURL(config)
+  config.headers = transformHeaders(config)
   config.data = transformRequestData(config)
 }
 
@@ -21,6 +23,11 @@ function transformURL(config: AxiosRequestConfig): string {
 
 function transformRequestData(config: AxiosRequestConfig): any {
   return transformRequest(config.data)
+}
+
+function transformHeaders(config: AxiosRequestConfig): any {
+  const { data, headers } = config
+  return processHeaders(headers, data)
 }
 
 export default axios
